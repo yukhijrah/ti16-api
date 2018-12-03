2fc1b9eb04c5c3f8b58160ef7cf7cccd5a3e4541
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-03 18:55:37 +0700

    initial commit 😎

---
+++ api/controllers/MahasiswaController.js
@@ -0,0 +1,19 @@
+/**
+ * MahasiswaController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    mahasiswas: function(req, res) {
+        Mahasiswa.find(function(err, mahasiswa) {
+            return res.json({
+                title: "Teknik Informatika",
+                year: 2016,
+                data: mahasiswa
+            });
+        });
+    }
+};
+
---
+++ api/models/Mahasiswa.js
@@ -0,0 +1,40 @@
+/**
+ * Mahasiswa.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  primaryKey: 'id',
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+   },
+   name: {
+      type: 'string'
+   },
+   nim: {
+     type: 'string'
+   }
+  },
+
+};
+
